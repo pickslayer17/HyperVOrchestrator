cc976919639c075d8e4601e09fa73b9a13b01105
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2026-07-18 22:09:06 +0300

    remove old scrypts

--- scripts/VM/30-GuestNetwork/02-Enable-Rdp.ps1
+++
@@ -1,9 +0,0 @@
-# enable rdp in guest
-
-$ScriptTarget = "VM"
-$ErrorActionPreference = "Stop"
-
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name UserAuthentication -Value 0
-netsh advfirewall firewall set rule group="Remote Desktop" new enable=yes
-Write-Host "RDP enabled."
--- scripts/VM/30-GuestNetwork/03-StartProxy.check.ps1
+++
@@ -1,8 +0,0 @@
-$ScriptTarget = "Host"
-$ErrorActionPreference = "Stop"
-
-$serverScript = "@@paths.pythonServer@@"
-& python "$serverScript" get_machine_names > $null 2>&1
-if ($LASTEXITCODE -eq 0) { Write-Host "proxy server already running."; exit 2 }
-Write-Host "proxy server not running."
-exit 0
