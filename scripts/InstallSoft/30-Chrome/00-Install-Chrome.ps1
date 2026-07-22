d2ef505aacee8e6f3d1c7b662a13af7dd94a9874
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2026-07-22 17:04:48 +0300

    chrome fix installation

--- scripts/InstallSoft/30-Chrome/00-Install-Chrome.ps1
+++ scripts/InstallSoft/30-Chrome/00-Install-Chrome.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 $installer = Join-Path $env:TEMP "chrome_enterprise.msi"
 $url = "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi"
 
-& curl.exe -L -o $installer $url
+& curl.exe -sS -L -o $installer $url 2>$null
 if ($LASTEXITCODE -ne 0) { throw "curl download failed (exit $LASTEXITCODE)" }
 $proc = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i","`"$installer`"","/qn","/norestart" -Wait -PassThru
 if ($proc.ExitCode -ne 0) { throw "msiexec failed (exit $($proc.ExitCode))" }
