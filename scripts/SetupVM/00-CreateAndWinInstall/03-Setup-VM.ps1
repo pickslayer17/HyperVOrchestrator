6b36b020654f2241662773096360aaf5cf8e15eb
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2026-07-22 16:46:54 +0300

    host -EnableEnhancedSessionMode

--- scripts/SetupVM/00-CreateAndWinInstall/03-Setup-VM.ps1
+++ scripts/SetupVM/00-CreateAndWinInstall/03-Setup-VM.ps1
@@ -8,7 +8,15 @@ $videoWidth = @@vm.videoWidth@@
 $videoHeight = @@vm.videoHeight@@
 $memoryGb = @@vm.memoryGb@@
 
+Set-VMHost -EnableEnhancedSessionMode $false
+Write-Host "Host EnableEnhancedSessionMode: $((Get-VMHost).EnableEnhancedSessionMode)"
+
 Set-VMMemory -VMName $vmName -DynamicMemoryEnabled $true -MinimumBytes 1GB -StartupBytes 1GB -MaximumBytes $memoryGb
 Set-VMVideo -VMName $vmName -HorizontalResolution $videoWidth -VerticalResolution $videoHeight -ResolutionType Single
-$transport = if ([enum]::GetNames([Microsoft.HyperV.PowerShell.EnhancedSessionTransportType]) -contains 'None') { 'None' } else { 'VMBus' }
+$transportParam = (Get-Command Set-VM).Parameters['EnhancedSessionTransportType'].ParameterType
+$transportEnum = if ($transportParam.IsGenericType) { $transportParam.GetGenericArguments()[0] } else { $transportParam }
+$transport = if ([enum]::GetNames($transportEnum) -contains 'None') { 'None' } else { 'VMBus' }
+Write-Host "EnhancedSessionTransportType: available=[$([enum]::GetNames($transportEnum) -join ', ')], selected=$transport"
 Set-VM -VMName $vmName -EnhancedSessionTransportType $transport
+Write-Host "VM '$vmName' configured: memory=$($memoryGb/1GB)GB, video=${videoWidth}x${videoHeight}, transport=$transport"
+Write-Host ""
